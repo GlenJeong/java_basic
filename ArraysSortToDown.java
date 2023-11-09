6d1cb160557a213694a319b56ab0d0d81f2485ae
Glenn Jeong <jyb1624@gmail.com>
2023-11-09 14:06:25 +0900

    feat: 5 크기의 배열을 만들고 랜덤으로 수를 입력받고 내림차순으로 정렬

---
+++ ArraysSortToDown.java
@@ -0,0 +1,30 @@
+package java_basic;
+
+import java.util.Arrays;
+
+public class ArraysSortToDown {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+
+        for (int i = 0; i < array.length; i++) {
+            int numRan = (int) (Math.random() * 10) + 1;
+            array[i] = numRan;
+        }
+
+        System.out.println(Arrays.toString(array));
+
+        int temp = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+
+    }
+
+}
