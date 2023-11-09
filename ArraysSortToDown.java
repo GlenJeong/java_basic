57c33860c3d2d5eff3449977bcf5d029264b0114
Glenn Jeong <jyb1624@gmail.com>
2023-11-09 14:07:51 +0900

    feat:  5크기의 배열을 만들고 수를 랜덤으로 받고 내림 정렬함

--- ArraysSortToDown.java
+++ ArraysSortToDown.java
@@ -16,7 +16,7 @@ public class ArraysSortToDown {
         int temp = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
-                if (array[i] > array[j]) {
+                if (array[i] < array[j]) {
                     temp = array[i];
                     array[i] = array[j];
                     array[j] = temp;
