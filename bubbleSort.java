75fd333d3432877a891f5053bbf5ba32d84901ee
Glenn Jeong <jyb1624@gmail.com>
2023-12-17 17:34:10 +0900

    feat: bubbleSort

---
+++ bubbleSort.java
@@ -0,0 +1,22 @@
+package java_basic;
+
+import java.util.*;;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        int[] arr = { 6, 3, 4, 8, 1, 5, 2, 7 };
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                int temp = 0;
+
+                if (arr[j] < arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
